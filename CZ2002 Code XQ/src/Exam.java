8823655d876eb102c5445f13b7343ec67987d5bc
Lim Xiao Qi <xqlim1996@gmail.com>
2018-11-06 20:07:31 +0800

    added xq source code

---
+++ CZ2002 Code XQ/src/Exam.java
@@ -0,0 +1,20 @@
+public class Exam {
+    private int Weight;
+    private int Mark;
+
+    public int getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(int weight) {
+        Weight = weight;
+    }
+
+    public int getMark() {
+        return Mark;
+    }
+
+    public void setMark(int mark) {
+        Mark = mark;
+    }
+}
---
+++ CZ2002 Code XQ/src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private String Name;
+    private int studentID;
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+}
